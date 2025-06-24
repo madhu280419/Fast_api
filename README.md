38dec78f3c2d7859ed3cd11cb740b3912ecd4a62
Madhu babu Yalavarthi <madhuyalavarthi1027@gmail.com>
2025-06-23 23:47:15 -0500

    car api

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Fast_api
+1.Created a New repository in github,then cloneit the repository in vs code.
+ [git clone (link)]
+
+2.created a env file 
+   [python -m venv env]
+
+3.Activated env file(virtual environment).
+  [env\Scripts\activate]
+
+4.installed  fastapi & uvicorn.
+   [pip3 install fastapi]
+   [pip3 install uvicorn]
+
+5.
--- main.py
+++ main.py
@@ -1,26 +1,21 @@
 import requests
+import json
+import os
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/Cars/")
-def get_car_details(model):
-    return {"message": "Welcome to the car info"}
+def get_car_details(model: str):
+    with open("data.json", "r") as json_file:
+        result = json.load(json_file)
+        total_result = result[model]["drive"][0]
+    
+    return total_result
 
 @app.post("/Cars/Info")
-async def add_car_details(model):
-    return {"Car details Received" }
-
-@app.put("/Cars/Update")
-async def update_car_details(year,drive ):
-    data = {
-        "year": year,
-        "drive":drive     
-        }
-    return {"message": "Car details updated", "data": data}
-
-    
-    url = 'https://api.api-ninjas.com/v1/cars?model={}'.format(model)
+async def add_car_details(model:str):
+    url = 'https://api.api-ninjas.com/v1/cars?model={}'.format(model,)
     payload = ""
     headers = {
         'X-Api-Key': 'a4f6SlPqvwrWa/xRiW1sGg==L32QYXtSlyISzGk3',  
@@ -29,21 +24,60 @@ async def update_car_details(year,drive ):
     response = requests.request("GET", url, headers=headers, data=payload)
 
     print("line number 23",response.status_code)
+    
     data = response.json()
     
-    result =  {data[0].get['make'],
-        data[0].get['year'],
-        data[0].get['transmission'],
-        data[0].get['fuel_type'],
-        data[0].get['model']
-            }
-        
+    result = {
+        data[0]['model']:{
+        "year": data[0]['year'],
+        "fuel_type": data[0]['fuel_type'],
+        "drive": data[0]['drive']
+    }}
+    if os.path.exists("data.json"):
+        with open("data.json", "r") as f:
+            try:
+                existing_data = json.load(f)
+            except json.JSONDecodeError:
+                existing_data = {}
+    else:
+        existing_data = {}
+
+# Update with new entry
+    existing_data.update(result)
+
+# Write back to file
+    with open("data.json", "w") as f:
+        json.dump(existing_data, f, indent=4)
+
     info = {"car details": result}
     return info
 
+   
+
+@app.put("/cars/Update/")
+async def update_car_details(model: str, year: int, drive: str):
+    with open("data.json", "r") as json_file:
+        data = json.load(json_file)
+    data = {
+        "year": year,
+        "drive": drive     
+        }
+    data.update({model:data})
+    if model in data:
+        data[model]["year"] = year,
+        data[model]["drive"] = drive,
+              
+        
+    with open("data.json", "w") as json_file:
+        json.dump(data, json_file, indent=4)
+        
+    
+    return {"message": "Car details updated", "data": data}
+
+ 
 @app.delete("/Cars/Delete")
 async def delete_car_details(drive):
     data = {
         "drive": drive
     }
-    return {"Car Details Deleted": data}
+    return {"Message": "Car Details Deleted", "data" : data}
